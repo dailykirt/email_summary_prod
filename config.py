8356aaa26a94d85563c357ffc4b1650e3586c577
Kirt Daily <dailykirt@gmail.com>
2019-11-17 23:18:32 -0600

    Website improvements

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'my very secret key'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     #local
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/testdb'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/testdb'
     #AWS
     #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://masterUsername:CraterLake12@rds-postgresql-email-sum.ced5yvd9vkk3.us-east-2.rds.amazonaws.com/myDatabase'
     #Heroku Enviroment URL
-    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://masterUsername:CraterLake12@rds-postgresql-email-sum.ced5yvd9vkk3.us-east-2.rds.amazonaws.com/myDatabase?sslrootcert=rds-combined-ca-bundle.pem&sslmode=require'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://masterUsername:CraterLake12@rds-postgresql-email-sum.ced5yvd9vkk3.us-east-2.rds.amazonaws.com/myDatabase?sslrootcert=rds-combined-ca-bundle.pem&sslmode=require'
     #EMAIL_TABLE = 'full_enron_emails'
     EMAIL_TABLE = 'test_rank_db'
     # EMAIL_TABLE = 'cleaned_sj'
